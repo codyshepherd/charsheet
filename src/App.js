c2ae5b6f240163e9ba2b2f4240fdbfd7644e26a9
Cody Shepherd <cody.shepherd@gmail.com>
2018-08-05 10:15:41 -0700

    Added tabbing classes and styles.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+
+import Tabs from './Tabs';
+require('./styles.css');
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Character Sheet</h1>
+        <Tabs>
+          <div label="Character">
+            Character info goes here.
+          </div>
+          <div label="Stats">
+            Stats info goes here.
+          </div>
+          <div label="Combat">
+            Combat info goes here.
+          </div>
+        </Tabs>
       </div>
     );
   }
