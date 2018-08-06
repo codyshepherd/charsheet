3d14bcc6d63bdeb09da4ad47ad8199598dd9d1e1
Cody Shepherd <cody.shepherd@gmail.com>
2018-08-06 14:14:56 -0700

    Toggling Edit now does so for all sheets.

--- src/Drawer.js
+++ src/Drawer.js
@@ -4,6 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 
 import SectionsList from './List';
+import EditCharInfoSheet from './EditCharInfoSheet';
 
 const drawerWidth = 240;
 
